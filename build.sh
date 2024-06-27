6bdf3e6b55e0714d6480096f74ad4937d43301e2
Michael Huang <michaelhly@gmail.com>
2024-06-26 21:35:21 -0400

    Refactor checkout paths

--- build.sh
+++ build.sh
@@ -3,10 +3,6 @@ set -ex
 apt-get update  -y \
     && apt-get install -y git wget vim numactl gcc-12 g++-12 python3 python3-pip libtcmalloc-minimal4 \
     && update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 10 --slave /usr/bin/g++ g++ /usr/bin/g++-12
+pip install -v -r requirements-cpu.txt --extra-index-url https://download.pytorch.org/whl/cpu
 
-echo 'export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4:$LD_PRELOAD' >> ~/.bashrc
-
-pip install -v -r source/requirements-cpu.txt --extra-index-url https://download.pytorch.org/whl/cpu
-
-cd source
 VLLM_TARGET_DEVICE=cpu python setup.py install
